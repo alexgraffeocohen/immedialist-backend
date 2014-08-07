8b5f81078be738bd1bff4cc41f27cbc8bddd1655
alexwilkinson <alex.wilkinson@me.com>
2014-08-07 15:42:41 -0400

    saved director factory

--- spec/factories/directors.rb
+++ spec/factories/directors.rb
@@ -2,11 +2,6 @@
 
 FactoryGirl.define do
   factory :director do
-    factory :cuaron do
-      name "Alfonso Cuaron"
-    end
-    factory :nolan do
-      name "Christopher Nolan"
-    end
+    name "Christopher Nolan"
   end
 end
