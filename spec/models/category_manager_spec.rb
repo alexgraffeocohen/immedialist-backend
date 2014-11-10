843e671a4967d3658ad1321f0e14318548c59655
alexwilkinson <alex.wilkinson@me.com>
2014-11-09 20:58:02 -0500

    revised, expanded category manager spec

--- spec/models/category_manager_spec.rb
+++ spec/models/category_manager_spec.rb
@@ -1,14 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe CategoryManager, :type => :model do
-  it 'can assign the movie category if model has movie(s)' do
+  it 'can assign the movie category if person has movie(s)' do
     actor = create(:person)
     movie = create(:movie)
     movie.actors << actor
-    category_manager = CategoryManager.new(actor)
-    category_manager.assign_categories
+    CategoryManager.new(actor).assign_categories
 
     expect(category_names_for(actor)).to include "Film"
+  end
+
+  it 'can assign the show category if person has show(s)' do
+    actor = create(:person)
+    show = create(:show)
+    show.actors << actor
+    CategoryManager.new(actor).assign_categories
+
+    expect(category_names_for(actor)).to include "Television"
+  end
+
+  it 'can assign the music category if model has album(s)' do
+    artist = create(:person)
+    album  = create(:album)
+    artist.albums << album
+    CategoryManager.new(artist).assign_categories
+
+    expect(category_names_for(artist)).to include "Music"
+  end
+
+  it 'can assign the book category if model has book(s)' do
+    author = create(:person)
+    book   = create(:book)
+    author.books << book
+    CategoryManager.new(author).assign_categories
+
+    expect(category_names_for(author)).to include "Book"
   end
 
   it 'will not assign the movie category if model has no movies' do
