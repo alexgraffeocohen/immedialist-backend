2368c264ba194c80be313d39f878d8e6b3604f6d
alexwilkinson <alex.wilkinson@me.com>
2014-12-08 00:25:48 -0500

    slight refactoring of MovieSearch spec

--- spec/models/movie_search_spec.rb
+++ spec/models/movie_search_spec.rb
@@ -6,6 +6,7 @@ describe MovieSearch do
   let(:movie_search_with_real_id)    { MovieSearch.new(external_id: real_id) }
   let(:movie_search_with_fake_id)    { MovieSearch.new(external_id: fake_id) }
   let(:real_title)                   { "The Matrix" }
+  let(:real_release_date)            { "1999" }
   let(:fake_title)                   { "When You Bite The Fry, the Fry Bites Back" }
   let(:real_id)                      { 603 }
   let(:fake_id)                      { 90909090 }
@@ -16,9 +17,9 @@ describe MovieSearch do
         VCR.use_cassette('real_title_movie_search') do
           result = movie_search_with_real_title.search
 
-          expect(result).to_not be_empty
+          expect(result).to be_an Array
           expect(result.first.title).to eq(real_title)
-          expect(result.first.release_date).to include("1999")
+          expect(result.first.release_date).to include(real_release_date)
           expect(result.first.id).to_not be_nil
         end
       end
@@ -27,23 +28,24 @@ describe MovieSearch do
         VCR.use_cassette('fake_title_movie_search') do
           result = movie_search_with_fake_title.search
 
-          expect(result).to eq([])
+          expect(result).to be_an Array
+          expect(result).to be_empty
         end
       end
     end
 
     context 'with an external id' do
-      it 'returns one movie with details' do
+      it 'returns a detailed movie record if external id is recognized' do
         VCR.use_cassette('real_id_movie_search') do
           result = movie_search_with_real_id.search
 
           expect(result.title).to eq(real_title)
-          expect(result.release_date).to include("1999")
+          expect(result.release_date).to include(real_release_date)
           expect(result.imdb_id).to_not be_nil
         end
       end
 
-      it 'returns itself if external id does not match' do
+      it 'returns an error object if external id is not recognized' do
         VCR.use_cassette('fake_id_movie_search') do
           result = movie_search_with_fake_id.search
 
