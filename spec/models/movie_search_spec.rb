eb26cd9f2a11e1e6afefebaf1b9fd10c6893f338
alexwilkinson <alex.wilkinson@me.com>
2014-12-11 18:58:34 -0500

    Add check for genres in movie_search_spec, real_release_date -> release_year

--- spec/models/movie_search_spec.rb
+++ spec/models/movie_search_spec.rb
@@ -6,7 +6,7 @@ describe MovieSearch do
   let(:movie_search_with_real_id)    { MovieSearch.new(external_id: real_id) }
   let(:movie_search_with_fake_id)    { MovieSearch.new(external_id: fake_id) }
   let(:real_title)                   { "The Matrix" }
-  let(:real_release_date)            { "1999" }
+  let(:release_year)                 { "1999" }
   let(:fake_title)                   { "When You Bite The Fry, the Fry Bites Back" }
   let(:real_id)                      { 603 }
   let(:fake_id)                      { 90909090 }
@@ -19,7 +19,7 @@ describe MovieSearch do
 
           expect(result).to be_an Array
           expect(result.first.title).to eq(real_title)
-          expect(result.first.release_date).to include(real_release_date)
+          expect(result.first.release_date).to include(release_year)
           expect(result.first.id).to_not be_nil
         end
       end
@@ -40,8 +40,9 @@ describe MovieSearch do
           result = movie_search_with_real_id.search
 
           expect(result.title).to eq(real_title)
-          expect(result.release_date).to include(real_release_date)
+          expect(result.release_date).to include(release_year)
           expect(result.imdb_id).to_not be_nil
+          expect(result.genres).to be_an Array
         end
       end
 
