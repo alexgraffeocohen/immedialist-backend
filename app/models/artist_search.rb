bbb86165e54ecd1ebda25ccc51c7f24817db705b
alexwilkinson <alex.wilkinson@me.com>
2014-12-20 16:19:38 -0500

    add ArtistSearch

---
+++ app/models/artist_search.rb
@@ -0,0 +1,11 @@
+class ArtistSearch < PersonSearch
+  private
+
+  def search_by_name
+    MusicQuerier.search_by_artist_name(name)
+  end
+
+  def search_by_external_id
+    MusicQuerier.search_by_artist_id(external_id)
+  end
+end
