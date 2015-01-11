aba2948efbce251f30c0ca4ca002d5266705beb1
alexwilkinson <alex.wilkinson@me.com>
2015-01-11 18:49:36 -0500

    add List model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111232038) do
+ActiveRecord::Schema.define(version: 20150111234733) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name"
@@ -99,6 +99,11 @@ ActiveRecord::Schema.define(version: 20150111232038) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "movie_actors", force: :cascade do |t|
