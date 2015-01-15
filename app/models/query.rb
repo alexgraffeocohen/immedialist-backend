bcb14e5dddbb0a284caf1858ca8b6682af490bb4
alexwilkinson <alex.wilkinson@me.com>
2015-01-15 13:38:27 -0500

    have Query inherit name from #list_item after create

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,5 +1,13 @@
 class Query < ActiveRecord::Base
   belongs_to :list_item
 
+  after_create :inherit_list_item_name
+
   validates_presence_of :list_item
+
+  private
+
+  def inherit_list_item_name
+    self.name = list_item.name
+  end
 end
