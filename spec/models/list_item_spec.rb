399e8af80389acb3cd1d55f719ae98565d2e7e0a
alexwilkinson <alex.wilkinson@me.com>
2015-01-15 11:38:46 -0500

    made list_item_spec a bit more efficient and clear

--- spec/models/list_item_spec.rb
+++ spec/models/list_item_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ListItem, :type => :model do
   let!(:film)     { FactoryGirl.create(:film_category) }
-  let(:list_item) { FactoryGirl.create(:list_item, category: film) }
+  let(:list_item) { FactoryGirl.build(:list_item, category: film) }
 
   it 'will receive the name of the item it is attached to' do
     item = FactoryGirl.create(:movie, name: 'Interstellar')
     list_item.item = item
     list_item.save
 
-    expect(list_item.item).to eq(item)
     expect(list_item.name).to eq(item.name)
   end
 end
