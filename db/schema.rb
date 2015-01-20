04b5f47ec63999ceeb18eaf7ea83ba4734f4ed3b
alexwilkinson <alex.wilkinson@me.com>
2015-01-19 22:49:38 -0500

    drop actors and directors tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120012502) do
-
-  create_table "actors", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20150120033839) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -84,12 +78,6 @@ ActiveRecord::Schema.define(version: 20150120012502) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "directors", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
