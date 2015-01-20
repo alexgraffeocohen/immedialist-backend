52d9d612ba4475bc6996c22df20f407e1d82f34a
alexwilkinson <alex.wilkinson@me.com>
2015-01-19 22:12:36 -0500

    add Search factory, clarify searches_spec

---
+++ spec/factories/searches.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :search do
+    name 'user search string'
+  end
+end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Search, :type => :model do
   let(:film_category) { create(:film_category) }
   let(:list_item) { build(:list_item, category: film_category) }
 
-  it 'inherits name from it\'s list item' do
-    search = Search.new(list_item: list_item)
+  it 'inherits name from it\'s list item on save' do
+    search = build(:search, list_item: list_item)
     search.save
 
     expect(search.name).to eq(list_item.name)
