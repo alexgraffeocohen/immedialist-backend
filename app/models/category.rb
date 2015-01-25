2419f3283bedecf218f009d9c6d031d31e089448
alexwilkinson <alex.wilkinson@me.com>
2015-01-25 16:51:05 -0500

    remove category_spec and Category#items

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,8 +5,4 @@ class Category < ActiveRecord::Base
   has_many :books
   has_many :people, through: :person_categories
   has_many :person_categories
-
-  def items
-    movies + songs + shows + books + people
-  end
 end
