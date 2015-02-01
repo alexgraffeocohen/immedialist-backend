12b8295bcd7b0f9f580499d48b94134114aef3fd
alexwilkinson <alex.wilkinson@me.com>
2015-01-31 21:09:20 -0500

    refactor SaveResultsFromQuery spec

--- spec/services/save_results_from_query_spec.rb
+++ spec/services/save_results_from_query_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe SaveResultsFromQuery, type: :service do
-  it 'calls the correct QuerySaver' do
+  let(:query)     { Query::Movie.new(name: "The Matrix") }
+  let(:item_type) { Immedialist::ItemType::Movie.new }
+
+  before(:each) do
     VCR.use_cassette('real_name_movie_query') do
-      query = Query::Movie.new(name: "The Matrix")
-      item_type = Immedialist::ItemType::Movie.new
-
       SaveResultsFromQuery.call(query, item_type)
-
-      the_matrix = Movie.find_by(name: "The Matrix")
-
-      expect(the_matrix).to_not be_nil
     end
+  end
+
+  it 'passes query results to the correct QuerySaver' do
+    the_matrix = Movie.find_by(name: "The Matrix")
+
+    expect(the_matrix).to_not be_nil
   end
 end
