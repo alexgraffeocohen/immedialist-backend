4073667f9d89445af0ad74187eb826ed11f3b5fb
alexwilkinson <alex.wilkinson@me.com>
2015-02-02 22:57:52 -0500

    refactor SaveResultsFromQuery to use QuerySanitizer and return early if needed

--- app/services/save_results_from_query.rb
+++ app/services/save_results_from_query.rb
@@ -10,10 +10,16 @@ class SaveResultsFromQuery
 
   def call
     query_results = @query.call
-    query_saver_class.call(query_results)
+    sanitized_results = query_sanitizer_class.call(query_results)
+    return [] if sanitized_results.empty?
+    query_saver_class.call(sanitized_results)
   end
 
   private
+
+  def query_sanitizer_class
+    QuerySanitizer.const_get(@item_type.name)
+  end
 
   def query_saver_class
     QuerySaver.const_get(@item_type.name)
--- spec/services/save_results_from_query_spec.rb
+++ spec/services/save_results_from_query_spec.rb
@@ -1,18 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe SaveResultsFromQuery, type: :service do
-  let(:query)     { Query::Movie.new(name: "The Matrix") }
   let(:item_type) { Immedialist::ItemType::Movie.new }
 
   before(:each) do
-    VCR.use_cassette('real_name_movie_query') do
-      SaveResultsFromQuery.call(query, item_type)
+    query_results
+  end
+
+  context 'when query has results' do
+    let(:query) { Query::Movie.new(name: "The Matrix") }
+    let(:query_results) do
+      VCR.use_cassette('real_name_movie_query') do
+        SaveResultsFromQuery.call(query, item_type)
+      end
+    end
+
+    it 'stores query results as objects in database' do
+      the_matrix = Movie.find_by(name: "The Matrix")
+
+      expect(the_matrix).to_not be_nil
+    end
+
+    it 'returns the objects it creates' do
+      expect(query_results).to be_an(Array)
+      expect(query_results.map(&:class).uniq).to eq([Movie])
     end
   end
 
-  it 'passes query results to the correct QuerySaver' do
-    the_matrix = Movie.find_by(name: "The Matrix")
+  context 'when query does not have results' do
+    let(:query) { Query::Movie.new(name: "Generic Query") }
+    let(:query_results) do
+      VCR.use_cassette('generic_list_item_query') do
+        SaveResultsFromQuery.call(query, item_type)
+      end
+    end
 
-    expect(the_matrix).to_not be_nil
+    it 'returns an empty array' do
+      expect(query_results).to be_an(Array)
+      expect(query_results).to be_empty
+    end
   end
 end
