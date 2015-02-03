b4f97b6c53510cef5a50ddf722125f883cd61302
alexwilkinson <alex.wilkinson@me.com>
2015-02-02 23:07:38 -0500

    expand upon spec to clarify generic case of any list item

--- spec/services/create_list_item_spec.rb
+++ spec/services/create_list_item_spec.rb
@@ -1,20 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe CreateListItem, type: :service do
-  let(:list_item)           { build(:list_item, name: "The Matrix") }
-  let(:requested_item_type) { "Movie" }
+  let(:search_results) { list_item.search.results }
 
-  before(:each) do
-    VCR.use_cassette('real_name_movie_query') do
-      CreateListItem.call(list_item, requested_item_type)
+  context 'given any list item' do
+    let(:list_item)           { build(:list_item, name: "Generic Query") }
+    let(:requested_item_type) { "Movie" }
+
+    before(:each) do
+      VCR.use_cassette('generic_list_item_query') do
+        CreateListItem.call(list_item, requested_item_type)
+      end
+    end
+
+    it 'saves the list item passed to it' do
+      expect(list_item).to be_persisted
     end
   end
 
-  it 'saves the list item passed to it' do
-    expect(list_item).to be_persisted
-  end
+  context 'when the list item name yields query results' do
+    let(:list_item)           { build(:list_item, name: "The Matrix") }
+    let(:requested_item_type) { "Movie" }
 
-  it 'attaches search results to list item' do
-    expect(list_item.search.results).to_not be_empty
+    before(:each) do
+      VCR.use_cassette('real_name_movie_query') do
+        CreateListItem.call(list_item, requested_item_type)
+      end
+    end
+
+    it 'attaches relevant results to the list item' do
+      expect(search_results).to_not be_empty
+      expect(search_results.map(&:class).uniq).to eq([Movie])
+
+      relevant_movie = search_results.select { |m| m.name == "The Matrix" }
+
+      expect(relevant_movie).to_not be_nil
+    end
   end
 end
