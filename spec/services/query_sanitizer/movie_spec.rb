07ef36b814e2e7f88225999495e39eeea415467a
alexwilkinson <alex.wilkinson@me.com>
2015-02-03 23:04:25 -0500

    refactor query_sanitizer/movie_spec to use helpers and expand the spec

--- spec/services/query_sanitizer/movie_spec.rb
+++ spec/services/query_sanitizer/movie_spec.rb
@@ -1,24 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe QuerySanitizer::Movie, type: :service do
-  let(:sanitized_results) { QuerySanitizer::Movie.call(query_results) }
-  let(:query_results) do
-    VCR.use_cassette('real_name_movie_query') do
-      query.call
-    end
-  end
-
   context 'when query has multiple results' do
     let(:query) { Query::Movie.new(name: "The Matrix") }
+    let(:query_results) { call_query_with_cassette('real_name_movie_query') }
 
-    it 'returns them in an array' do
-      expect(sanitized_results).to be_an(Array)
-    end
+    it_behaves_like "a query sanitizer with results"
+  end
 
-    it 'converts each result into a hash' do
-      movie_attributes = sanitized_results.first
+  context 'when query has no results' do
+    let(:query) { Query::Movie.new(name: "There Are No Results For Sure") }
+    let(:query_results) { call_query_with_cassette('no_results_movie_query') }
 
-      expect(movie_attributes).to be_a(Hash)
-    end
+    it_behaves_like "a query sanitizer with no results"
   end
 end
