a2e95197c11f7ef8b07adb69f7fa16f25b645d92
alexwilkinson <alex.wilkinson@me.com>
2015-02-06 23:17:55 -0500

    modify query/movie_spec and query_sanitizer/movie_spec to use TestQuery class

--- spec/models/query/movie_spec.rb
+++ spec/models/query/movie_spec.rb
@@ -1,57 +1,43 @@
 require 'rails_helper'
 
 describe Query::Movie do
-  let(:movie_query_with_real_name)  { Query::Movie.new(name: real_name) }
-  let(:movie_query_with_fake_name)  { Query::Movie.new(name: non_existant_name) }
-  let(:movie_query_with_real_id)    { Query::Movie.new(external_id: real_id) }
-  let(:movie_query_with_fake_id)    { Query::Movie.new(external_id: fake_id) }
-  let(:real_name)                   { "The Matrix" }
-  let(:release_year)                { "1999" }
-  let(:non_existant_name)           { "There Are No Results For Sure" }
-  let(:real_id)                     { 603 }
-  let(:fake_id)                     { 90909090 }
+  let(:test_query) { TestQuery::Movie }
+  let(:real_movie) { build(:real_movie) }
+  let(:fake_movie) { build(:fake_movie) }
 
   describe '#call' do
     context 'with a name' do
       it 'returns an array of movie results if there is a match' do
-        VCR.use_cassette('real_name_movie_query') do
-          result = movie_query_with_real_name.call
+        result = test_query.call_with_real_name
 
-          expect(result).to be_an Array
-          expect(result.first.title).to eq(real_name)
-          expect(result.first.release_date).to include(release_year)
-          expect(result.first.id).to_not be_nil
-        end
+        expect(result).to be_an Array
+        expect(result.first.title).to eq(real_movie.name)
+        expect(result.first.release_date).to include(real_movie.release_date.to_s)
+        expect(result.first.id).to_not be_nil
       end
 
       it 'returns an empty array if there is no match' do
-        VCR.use_cassette('no_results_movie_query') do
-          result = movie_query_with_fake_name.call
+        result = test_query.call_with_fake_name
 
-          expect(result).to be_an Array
-          expect(result).to be_empty
-        end
+        expect(result).to be_an Array
+        expect(result).to be_empty
       end
     end
 
     context 'with an external id' do
       it 'returns a detailed movie record if external id is recognized' do
-        VCR.use_cassette('real_id_movie_query') do
-          result = movie_query_with_real_id.call
+        result = test_query.call_with_real_id
 
-          expect(result.title).to eq(real_name)
-          expect(result.release_date).to include(release_year)
-          expect(result.imdb_id).to_not be_nil
-          expect(result.genres).to be_an Array
-        end
+        expect(result.title).to eq(real_movie.name)
+        expect(result.release_date).to include(real_movie.release_date.to_s)
+        expect(result.imdb_id).to_not be_nil
+        expect(result.genres).to be_an Array
       end
 
       it 'returns an error object if external id is not recognized' do
-        VCR.use_cassette('fake_id_movie_query') do
-          result = movie_query_with_fake_id.call
+        result = test_query.call_with_fake_id
 
-          expect(result[:status_message]).to include("Invalid id")
-        end
+        expect(result[:status_message]).to include("Invalid id")
       end
     end
   end
--- spec/services/query_sanitizer/movie_spec.rb
+++ spec/services/query_sanitizer/movie_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe QuerySanitizer::Movie, type: :service do
-  let(:movie_tester) { MovieTester.new }
+  let(:test_query) { TestQuery::Movie }
 
   context 'when query has multiple results' do
-    let(:query_results) { movie_tester.call_query_with_results }
+    let(:query_results) { test_query.call_with_real_name }
 
     it_behaves_like "a query sanitizer with results"
   end
 
   context 'when query has no results' do
-    let(:query_results) { movie_tester.call_query_with_no_results }
+    let(:query_results) { test_query.call_with_fake_name }
 
     it_behaves_like "a query sanitizer with no results"
   end
