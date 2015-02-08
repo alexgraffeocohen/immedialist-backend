3a94e02b7f682375dd586e58fc4f8883e9437e4d
alexwilkinson <alex.wilkinson@me.com>
2015-02-08 00:53:02 -0500

    add QuerySanitizer::Song

---
+++ app/services/query_sanitizer/song.rb
@@ -0,0 +1,13 @@
+class QuerySanitizer::Song < QuerySanitizer
+  private
+
+  def compare_results_to_api_expectations
+    results.is_a?(Array) or raise TypeError, "Results is not an array"
+    return [] if results.empty?
+    results.first.is_a?(RSpotify::Track) or raise TypeError, "Result is not a Spotify Track object"
+  end
+
+  def sanitize_results
+    results.map(&:as_json).map(&:symbolize_keys)
+  end
+end
