9ed52982739475c96715daf6442d9f8f8ab9cc26
alexwilkinson <alex.wilkinson@me.com>
2015-02-08 01:04:10 -0500

    make fake_song factory more accurate

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -17,10 +17,6 @@ FactoryGirl.define do
     factory :fake_song do
       name "Even The Hipsters Don't Know This Song It's So Obscure"
       spotify_id "ithinkicanithinkicanithinkican"
-      duration_ms 207000
-      spotify_preview_url "//p.scdn.co/mp3-preview/9bf606134888aa3a0b2c47e19a75d175c3684609"
-      spotify_url "https://open.spotify.com/track/6aUAF8JOd8zEl41B6I18xL"
-      spotify_popularity 61
     end
   end
 end
