6e8ed6a04ca8339c58c7f97474c45c3505ad3db1
alexwilkinson <alex.wilkinson@me.com>
2015-02-08 00:34:31 -0500

    add QuerySaver::Director

---
+++ app/services/query_saver/director.rb
@@ -0,0 +1,17 @@
+class QuerySaver::Director < QuerySaver
+  def call
+    directors = build_directors
+    directors.each(&:save!)
+  end
+
+  private
+
+  def build_directors
+    results.map do |director_result|
+      ::Person.new(
+        name: director_result.fetch(:name),
+        tmdb_id: director_result.fetch(:id)
+      )
+    end
+  end
+end
---
+++ spec/services/query_saver/director_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe QuerySaver::Director, type: :service do
+  let(:query_results) { TestQuery::Director.new.call_with_real_name }
+  let(:sanitized_query_results) { QuerySanitizer::Director.call(query_results) }
+
+  it_behaves_like 'a query saver', Person, FactoryGirl.build(:real_director)
+end
