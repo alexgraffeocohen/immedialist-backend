ea706ff8f94b5fd23c67ebed56960629e374e226
alexwilkinson <alex.wilkinson@me.com>
2015-02-08 00:22:23 -0500

    use query_saver shared examples in query_saver/movie_spec

--- spec/services/query_saver/movie_spec.rb
+++ spec/services/query_saver/movie_spec.rb
@@ -1,27 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuerySaver::Movie, type: :service do
-  let(:query)  { Query::Movie.new(name: "The Matrix") }
-  let(:movies) { QuerySaver::Movie.call(sanitized_query_results) }
-  let(:sanitized_query_results) do
-    VCR.use_cassette('real_name_movie_query') do
-      QuerySanitizer::Movie.call(query.call)
-    end
-  end
+  let(:query_results) { TestQuery::Movie.new.call_with_real_name }
+  let(:sanitized_query_results) { QuerySanitizer::Movie.call(query_results) }
 
-  before(:each) do
-    movies
-  end
-
-  it 'returns Movie objects based on query data' do
-    expect(movies).to be_an(Array)
-    expect(movies.first).to be_a(Movie)
-  end
-
-  it 'saves the Movie objects' do
-    the_matrix = Movie.find_by(tmdb_id: 603)
-
-    expect(the_matrix.name).to eq('The Matrix')
-    expect(the_matrix.release_date.year).to eq(1999)
-  end
+  it_behaves_like 'a query saver', Movie, FactoryGirl.build(:real_movie)
 end
