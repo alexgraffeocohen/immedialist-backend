846248436783bac8434789c8db68b5f26551805f
alexwilkinson <alex.wilkinson@me.com>
2015-02-09 22:09:34 -0500

    fetch cover url for album in QuerySaver::Album

--- app/services/query_saver/album.rb
+++ app/services/query_saver/album.rb
@@ -11,8 +11,19 @@ class QuerySaver::Album < QuerySaver
       ::Album.new(
         name: album_result.fetch(:name),
         release_date: album_result.fetch(:release_date),
-        spotify_id: album_result.fetch(:id)
+        spotify_id: album_result.fetch(:id),
+        cover_url: grab_cover_url(album_result)
       )
+    end
+  end
+
+  def grab_cover_url(album_result)
+    images = album_result.fetch(:images)
+
+    if images
+      images.sort { |h, other_h| other_h[:height] <=> h[:height] }.
+        first.
+        fetch(:url)
     end
   end
 end
