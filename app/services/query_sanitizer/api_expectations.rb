34d0809e3757ef003aaaad2bec3f42438381a54c
alexwilkinson <alex.wilkinson@me.com>
2015-02-12 00:17:25 -0500

    (m) shorten lines in ApiExpectations

--- app/services/query_sanitizer/api_expectations.rb
+++ app/services/query_sanitizer/api_expectations.rb
@@ -2,10 +2,12 @@ module QuerySanitizer::ApiExpectations
   module_function
 
   def tmdb_api_expectations_for(tmdb_type)
-    results.first.is_a?(Tmdb.const_get(tmdb_type)) or raise TypeError, "Result is not a TMDB #{tmdb_type} object"
+    results.first.is_a?(Tmdb.const_get(tmdb_type)) or
+      raise TypeError, "Result is not a TMDB::#{tmdb_type} object"
   end
 
   def spotify_api_expectations_for(spotify_type)
-    results.first.is_a?(RSpotify.const_get(spotify_type)) or raise TypeError, "Result is not a Spotify Track object"
+    results.first.is_a?(RSpotify.const_get(spotify_type)) or
+      raise TypeError, "Result is not a Spotify::#{spotify_type} object"
   end
 end
