78e9b6124f6fc6b5e3a0c53eee2454465426f8a1
alexwilkinson <alex.wilkinson@me.com>
2015-02-12 00:08:09 -0500

    ensure results are an Array on QuerySanitizer#initialize and return if it's empty before checking api expectations

--- app/services/query_sanitizer/api_expectations.rb
+++ app/services/query_sanitizer/api_expectations.rb
@@ -2,14 +2,10 @@ module QuerySanitizer::ApiExpectations
   module_function
 
   def tmdb_api_expectations_for(tmdb_type)
-    results.is_a?(Array) or raise TypeError, "Results is not an array"
-    return [] if results.empty?
     results.first.is_a?(Tmdb.const_get(tmdb_type)) or raise TypeError, "Result is not a TMDB #{tmdb_type} object"
   end
 
   def spotify_api_expectations_for(spotify_type)
-    results.is_a?(Array) or raise TypeError, "Results is not an array"
-    return [] if results.empty?
     results.first.is_a?(RSpotify.const_get(spotify_type)) or raise TypeError, "Result is not a Spotify Track object"
   end
 end
