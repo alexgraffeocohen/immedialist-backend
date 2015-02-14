f1a473f961c8cb19f7eabb5e6d6f70d454586e50
alexwilkinson <alex.wilkinson@me.com>
2015-02-13 22:20:55 -0500

    make it blocks for book query spec accurate

--- spec/services/query/book_spec.rb
+++ spec/services/query/book_spec.rb
@@ -7,14 +7,14 @@ describe Query::Book do
 
   describe '#call' do
     context 'with a name' do
-      it 'returns an array of book results if there is a match' do
+      it 'returns a Hashie object with an array of book results' do
         result = test_query.call_with_real_name
 
         expect(result.results.work).to be_an(Array)
         expect(result.results.work.first.best_book.title).to eq(real_book.name)
       end
 
-      it 'returns an empty array if there is no match' do
+      it 'retuns a Hashie object with total results set to zero when there are no results' do
         result = test_query.call_with_fake_name
 
         expect(result.total_results).to eq('0')
@@ -29,7 +29,7 @@ describe Query::Book do
         expect(result.isbn).to eq(real_book.isbn)
       end
 
-      it 'returns an error object if external id is not recognized' do
+      it 'raises a NoMethodError when external id is not recognized' do
         expect { test_query.call_with_fake_id }.to raise_exception(NoMethodError)
       end
     end
