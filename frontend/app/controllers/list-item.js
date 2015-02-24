8b7e19794879705f95aaeb721a4c0e69fea80fea
alexwilkinson <alex.wilkinson@me.com>
2015-02-24 00:47:14 -0500

    add more type and color handling on list-item controller

--- frontend/app/controllers/list-item.js
+++ frontend/app/controllers/list-item.js
@@ -9,8 +9,23 @@ export default Ember.ObjectController.extend({
       case "actor":
         return "fa-user"
       break;
+      case "director":
+        return "fa-user"
+      break;
+      case "author":
+        return "fa-user"
+      break;
+      case "artist":
+        return "fa-user"
+      break;
       case "song":
         return "fa-music"
+      break;
+      case "album":
+        return "fa-music"
+      break;
+      case "book":
+        return "fa-book"
       break;
     }
   }.property('model.type'),
@@ -21,6 +36,12 @@ export default Ember.ObjectController.extend({
       break;
       case "song":
         return "list-group-item-success"
+      break;
+      case "album":
+        return "list-group-item-success"
+      break;
+      case "book":
+        return "list-group-item-danger"
       break;
     }
   }.property('model.type')
