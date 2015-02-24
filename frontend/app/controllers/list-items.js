69d3327046deffecde18e38bc8a4e71660d29f02
alexwilkinson <alex.wilkinson@me.com>
2015-02-24 00:42:38 -0500

    hook up action for creating list items

--- frontend/app/controllers/list-items.js
+++ frontend/app/controllers/list-items.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  actions: {
+    createListItem: function(newListItem) {
+      var listItem = this.store.createRecord('listItem', newListItem);
+      this.set('newListItem', {type: newListItem.type});
+      listItem.save();
+    }
+  },
+  newListItem: {},
   types: [
     "movie",
     "actor",
