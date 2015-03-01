c5f68b5bbf5b86e4bc8c315f74cb9c14a1595848
alexwilkinson <alex.wilkinson@me.com>
2015-02-28 21:00:59 -0500

    make list_item_spec faster and more robust

--- spec/models/list_item_spec.rb
+++ spec/models/list_item_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe ListItem, :type => :model do
   let(:list_item) { FactoryGirl.build(:list_item) }
 
+  it { should have_one(:search) }
+  it { should belong_to(:user) }
+  it { should belong_to(:item) }
+
   it 'will receive the name of the item it is attached to' do
-    item = FactoryGirl.create(:movie, name: 'Interstellar')
+    item = FactoryGirl.build(:movie, name: 'Interstellar')
     list_item.item = item
     list_item.save
 
