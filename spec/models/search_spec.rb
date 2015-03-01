1be98de3fb6f993cd48718f50108948ee4e04289
alexwilkinson <alex.wilkinson@me.com>
2015-02-28 23:16:36 -0500

    (m) remove unneeded let

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Search, :type => :model do
-  let(:list_item) { create(:list_item) }
-
   it { should belong_to(:list_item) }
 
   it_behaves_like "it can be converted to an item type", Search::Movie.new
 
   it 'inherits name from it\'s list item on save' do
+    list_item = FactoryGirl.create(:list_item)
     search = build(:search, list_item: list_item)
     search.save
 
