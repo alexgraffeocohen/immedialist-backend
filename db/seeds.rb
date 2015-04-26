8f7ff3a7696a8676bbc69a251ae170ff4b3a6681
Alex Wilkinson <alex.wilkinson@me.com>
2015-04-26 17:48:39 -0400

    add test to make sure all types of saved list items can be read

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,15 @@
 die_hard = Movie.create(name: "Die Hard")
 the_hobbit = Book.create(name: "The Hobbit")
 reflektor = Album.create(name: "Reflektor")
+lucky_you = Song.create(name: "Lucky You")
+brad_pitt = Creator.create(name: "Brad Pitt")
+lost = Show.create(name: "Lost")
 
 ListItem.create([
   { name: "Die Hard", item: die_hard  },
   { name: "The Hobbit", item: the_hobbit },
-  { name: "Reflektor", item: reflektor }
+  { name: "Reflektor", item: reflektor },
+  { name: "Lucky You", item: lucky_you },
+  { name: "Brad Pitt", item: brad_pitt },
+  { name: "Lost", item: lost }
 ])
--- spec/features/list_item_lifecycle_spec.rb
+++ spec/features/list_item_lifecycle_spec.rb
@@ -2,24 +2,35 @@ require 'rails_helper'
 
 describe 'the list item lifecycle', type: :feature, js: true do
   let(:categories) { [
-    "Movie",
-    "Show",
-    "Actor",
-    "Director",
-    "Song",
-    "Album",
-    "Artist",
-    "Book",
-    "Author"
+    "movie",
+    "show",
+    "actor",
+    "director",
+    "song",
+    "album",
+    "artist",
+    "book",
+    "author"
   ] }
 
-  it 'allows for creating and deleting of all list item types' do
+  it 'allows for reading, creating, and deleting of all list item types' do
+    Rails.application.load_seed
+
     visit '/'
 
+    within '.list' do
+      expect(page).to have_content('Die Hard')
+      expect(page).to have_content('The Hobbit')
+      expect(page).to have_content('Reflektor')
+      expect(page).to have_content('Lucky You')
+      expect(page).to have_content('Brad Pitt')
+      expect(page).to have_content('Lost')
+    end
+
     categories.each do |category|
-      VCR.use_cassette("item_lifecycle_#{category.downcase}") do
+      VCR.use_cassette("item_lifecycle_#{category}") do
         within 'form.create' do
-          select(category)
+          select(category.titleize)
           fill_in('list-item-name', with: 'Media Title')
           click_button('Add To List')
         end
@@ -30,7 +41,7 @@ describe 'the list item lifecycle', type: :feature, js: true do
 
         wait_for_ajax
 
-        within ".#{category.downcase}.media-title" do
+        within ".#{category}.media-title" do
           click_button('list-item-delete')
         end
 
