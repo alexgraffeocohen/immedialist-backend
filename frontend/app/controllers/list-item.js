d73e1722ae9f5224b956391af66cac0142390412
Alex Wilkinson <alex.wilkinson@me.com>
2015-04-27 00:08:13 -0400

    upgrade to ember-1.11 and address deprecations around ObjectController

--- frontend/app/controllers/list-item.js
+++ frontend/app/controllers/list-item.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   actions: {
     deleteListItem: function() {
       var listItem = this.get('model');
