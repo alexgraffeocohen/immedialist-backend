123fd36ebbca2d8f9788a923dd2c51d8ec4b7da5
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-02 21:39:18 -0400

    RequestedItemSerializer: use ItemType#associated_class to return the correct media_type

--- app/serializers/requested_item_serializer.rb
+++ app/serializers/requested_item_serializer.rb
@@ -2,6 +2,6 @@ class RequestedItemSerializer < ItemSerializer
   # FIXME: I feel like this is unecessary, but I'm unsure of the consequences
   # of taking up #media_type right now
   def media_type
-    object.media_name.downcase
+    object.to_item_type.associated_class.name.downcase
   end
 end
