d497b6a18f0e7c06970c99cb5e192548111865fc
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-03 00:00:01 -0400

    RequestedItemSerializer: stop inheriting from ItemSerializer to correctly include #requested_type

--- app/serializers/requested_item_serializer.rb
+++ app/serializers/requested_item_serializer.rb
@@ -1,6 +1,6 @@
-class RequestedItemSerializer < ItemSerializer
-  # FIXME: I feel like this is unecessary, but I'm unsure of the consequences
-  # of taking up #media_type right now
+class RequestedItemSerializer < ActiveModel::Serializer
+  attributes :id, :name, :requested_type, :media_type
+
   def media_type
     object.to_item_type.associated_class.name.downcase
   end
