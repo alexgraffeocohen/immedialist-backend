d9380aedac0307cbadd80ab082c3caa6fddc577a
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-08 00:05:28 -0400

    RequestedItemSerializer: explicitly downcase #requested_type.

--- app/serializers/requested_item_serializer.rb
+++ app/serializers/requested_item_serializer.rb
@@ -1,6 +1,10 @@
 class RequestedItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :requested_type, :media_type
 
+  def requested_type
+    object.requested_type.downcase
+  end
+
   def media_type
     object.to_item_type.associated_class.name.downcase
   end
