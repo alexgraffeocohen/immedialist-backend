bcf53bddd9b6ac214e38923870fe5ba1c528c221
Alex Wilkinson <alex.wilkinson@me.com>
2016-01-16 18:44:41 -0500

    ListItem Request Specs: add coverage around not passing name

--- spec/requests/list_items_spec.rb
+++ spec/requests/list_items_spec.rb
@@ -26,6 +26,26 @@ RSpec.describe "ListItems", type: :request do
       end
     end
 
+    context "without a name" do
+      it "fails to create a ListItem" do
+        # We have to create a RequestedItem in order for the ListItem post to
+        # be successful. Any front end client has to first post to
+        # /requested_items.
+        requested_movie = FactoryGirl.create(:requested_item,
+                                             name: "Awesome Movie",
+                                             requested_type: "Movie")
+
+        post "/list_items",
+          { list_item: { name: nil,
+                         item_type: "RequestedItem",
+                         item_id: requested_movie.id } },
+          headers
+
+        expect(response.content_type).to eq("application/json")
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
     context "without a RequestedItem id" do
       it "fails to create a ListItem" do
         post "/list_items",
