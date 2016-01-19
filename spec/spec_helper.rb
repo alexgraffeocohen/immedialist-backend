10fb8311a03493e35b00d135c60c78db1b053539
Alex Wilkinson <alex.wilkinson@me.com>
2016-01-18 23:04:19 -0500

    Verify doubled constant names in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,5 +31,6 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.syntax = :expect
     mocks.verify_partial_doubles = true
+    mocks.verify_doubled_constant_names = true
   end
 end
