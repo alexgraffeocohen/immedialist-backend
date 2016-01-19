a22c9f5c2950ca597f4eb60690602ef7045781a8
Alex Wilkinson <alex.wilkinson@me.com>
2016-01-19 00:05:20 -0500

    ListItems Factory: Make it easier to add a search

--- spec/factories/list_items.rb
+++ spec/factories/list_items.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :list_item do
     name 'List Item Name'
     association :item, factory: :requested_item, strategy: :build
+
+    trait :with_search do
+      association(:search)
+    end
   end
 end
