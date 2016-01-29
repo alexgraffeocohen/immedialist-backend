6adffa2fe321fc37e0e70661b3e4b49b6cb3c267
Alex Wilkinson <alex.wilkinson@me.com>
2016-01-28 23:08:21 -0500

    Refactor UpdateItem::Movie: extract methods up to UpdateItem

--- app/services/update_item.rb
+++ app/services/update_item.rb
@@ -18,4 +18,44 @@ class UpdateItem
   def update_item!
     raise NotImplementedError
   end
+
+  def update_genres!(api_resource, api_identifier)
+    join_model_name = "#{item.class.name.downcase}_genres"
+
+    api_resource.genres.each do |association_record|
+      db_record = Genre.find_by(
+        api_identifier => association_record.send(api_identifier)
+      )
+
+      if db_record
+        item.send(join_model_name).find_or_create_by!(genre: db_record)
+      else
+        item.genres << Genre.create!(association_record.attributes)
+      end
+    end
+  end
+
+  def update_actors!(api_resource, api_identifier)
+    update_creator_association!(api_resource, api_identifier, "actors")
+  end
+
+  def update_directors!(api_resource, api_identifier)
+    update_creator_association!(api_resource, api_identifier, "directors")
+  end
+
+  def update_creator_association!(api_resource, api_identifier, association_name)
+    join_model_name = "#{item.class.name.downcase}_#{association_name}"
+
+    api_resource.send(association_name).each do |association_record|
+      db_record = Creator.find_by(
+        api_identifier => association_record.send(api_identifier)
+      )
+
+      if db_record
+        item.send(join_model_name).find_or_create_by!(creator: db_record)
+      else
+        item.send(association_name) << Creator.create!(association_record.attributes)
+      end
+    end
+  end
 end
