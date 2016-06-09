c248fb9f87ffeeb5677eb3c09a4f0d16a79385e8
Alex Wilkinson <alex.wilkinson@me.com>
2016-06-09 15:16:33 -0400

    Push #update_genres! down to UpdateItem::Movie and UpdateItem::Show, since it's TMDB specific
    Spotify does not have an API resource for a Genre, for example

--- app/services/update_item/show.rb
+++ app/services/update_item/show.rb
@@ -11,4 +11,20 @@ class UpdateItem::Show < UpdateItem
   def tmdb_show
     @tmdb_show ||= Immedialist::TMDB::Show.find(item.tmdb_id)
   end
+
+  def update_genres!(api_resource, api_identifier)
+    join_model_name = "#{item.class.name.downcase}_genres"
+
+    api_resource.genres.each do |association_record|
+      db_record = Genre.find_by(
+        api_identifier => association_record.send(api_identifier)
+      )
+
+      if db_record
+        item.send(join_model_name).find_or_create_by!(genre: db_record)
+      else
+        item.genres << Genre.create!(association_record.attributes)
+      end
+    end
+  end
 end
