5fae99fb95abaf208514797708efa2771e68a867
Alex Wilkinson <alex.wilkinson@me.com>
2016-07-15 12:40:21 -0400

    (m) Whitespace

--- app/services/update_item/creator.rb
+++ app/services/update_item/creator.rb
@@ -23,28 +23,40 @@ class UpdateItem::Creator < UpdateItem
   end
 
   def tmdb_person
-    @tmdb_person ||= Immedialist::TMDB::Person.find(item.tmdb_id)
+    @tmdb_person ||= Immedialist::TMDB::Person.find(
+      item.tmdb_id
+    )
   end
 
   def spotify_artist
-    @spotify_artist ||= Immedialist::Spotify::Artist.find(item.spotify_id)
+    @spotify_artist ||= Immedialist::Spotify::Artist.find(
+      item.spotify_id
+    )
   end
 
   def update_movies_acted_in!
-    update_tmdb_association!(::Movie, :movie_actors, :movies_acted_in)
+    update_tmdb_association!(
+      ::Movie, :movie_actors, :movies_acted_in
+    )
   end
 
   def update_movies_directed!
-    update_tmdb_association!(::Movie, :movie_directors, :movies_directed)
+    update_tmdb_association!(
+      ::Movie, :movie_directors, :movies_directed
+    )
   end
 
   def update_shows_acted_in!
-    update_tmdb_association!(::Show, :show_actors, :shows_acted_in)
+    update_tmdb_association!(
+      ::Show, :show_actors, :shows_acted_in
+    )
   end
 
   def update_tmdb_association!(media_class, join_model_name, association_name)
     tmdb_person.send(association_name).each do |tmdb_movie|
-      movie_in_db = media_class.find_by(tmdb_id: tmdb_movie.tmdb_id)
+      movie_in_db = media_class.find_by(
+        tmdb_id: tmdb_movie.tmdb_id
+      )
 
       if movie_in_db
         item.send(join_model_name).find_or_create_by!(
